e500f2ad7460ebc3fd0fe9ca4cdbdf3a3bdc4500
Eason <hao@haolin10.cn>
2024-06-10 16:35:55 +0800

    Start

---
+++ src/main/java/yj/hometeach/HomeTeachApplication.java
@@ -0,0 +1,13 @@
+package yj.hometeach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTeachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeTeachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yj/hometeach/HomeTeachApplicationTests.java
@@ -0,0 +1,13 @@
+package yj.hometeach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTeachApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
